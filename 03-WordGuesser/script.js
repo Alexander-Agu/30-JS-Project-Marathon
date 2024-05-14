dba506dcad6df31ed82ab15954fc809b4949b1ee
Alexander Agu <theonlyagu@gmail.com>
2024-05-14 12:47:24 +0200

    finished giving Word Guessser functionality

--- 03-WordGuesser/script.js
+++ 03-WordGuesser/script.js
@@ -1,57 +1,63 @@
 // Create a word guesser game
 // There are 6 boxes containing 6 scaterd letter for a full 6 letter word
 // The letters are hidden within the boxes
-// Player will have a limit of revealing 3 of the 6 letters to give them the clue on the word they need to guess
+// Player will have a limit of revealing 4 of the 6 letters to give them the clue on the word they need to guess
+
 
 // programming the buttons to reveal the letters once clicked
-let buttonClicked = 0;
+// A max of 4 buttons can be clicked
+function revealLetters() {
+    let buttonClicked = 0;
 
-function revealLetters(){
-
-    function clickCounter(){
-        buttonClicked++;
-        console.log(buttonClicked)
-        if(buttonClicked === 3){
-            console.log("stop")
-        }
+    function displayLetter(buttonId, letter) {
+        document.getElementById(buttonId).textContent = letter;
     }
 
-    function revealedLetters(){
-        let btn1 = document.getElementById("btn-1").onclick = function(){
-            document.getElementById("btn-1").textContent = "E";
-            clickCounter()
-        };
-        let btn2 = document.getElementById("btn-2").onclick = function(){
-            document.getElementById("btn-2").textContent = "T";
-            clickCounter()
-        };
-        let btn3 = document.getElementById("btn-3").onclick = function(){
-            document.getElementById("btn-3").textContent = "H";
-            clickCounter()
-        };
-        let btn4 = document.getElementById("btn-4").onclick = function(){
-            document.getElementById("btn-4").textContent = "A";
-            clickCounter()
-        };
-        let btn5 = document.getElementById("btn-5").onclick = function(){
-            document.getElementById("btn-5").textContent = "R";
-            clickCounter()
-        };
-        let btn6 = document.getElementById("btn-6").onclick = function(){
-            document.getElementById("btn-6").textContent = "F";
-            clickCounter()
+    function handleButtonClick(buttonId, letter) {
+        document.getElementById(buttonId).onclick = function () {
+            if (buttonClicked < 4) {
+                displayLetter(buttonId, letter);
+                buttonClicked++;
+                console.log(buttonClicked);
+                if (buttonClicked >= 3) {
+                    console.log("Max clicks reached");
+                }
+            } else {
+                console.log("Max clicks reached");
+            }
         };
     }
 
-    revealedLetters()
 
+    const buttons = [
+        { id: "btn-1", letter: "E" },
+        { id: "btn-2", letter: "T" },
+        { id: "btn-3", letter: "H" },
+        { id: "btn-4", letter: "A" },
+        { id: "btn-5", letter: "R" },
+        { id: "btn-6", letter: "F" }
+    ];
 
-
-
-
+    // Attach click handlers to buttons
+    buttons.forEach(button => {
+        handleButtonClick(button.id, button.letter);
+    });
 }
 
-revealLetters()
+revealLetters();
 
+// Using the revealed letters you must get the correct answer and submit it
+let answer = "";
+document.getElementById("submit").onclick = function(){
+    answer = "father"
+    answer = answer.toLocaleUpperCase();
 
+    let userAnswer = document.getElementById("answer").value;
+    userAnswer = userAnswer.toLocaleUpperCase();
 
+    if(userAnswer == answer){
+        document.getElementById("results").textContent = `You got it corrects`
+    } else{
+        document.getElementById("results").textContent = `HINT: Its something black kids dont have from the moments they are born`
+    }
+}
