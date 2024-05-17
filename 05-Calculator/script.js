b65f49b634452022076cdc5ef3e0a1c7bae98592
misterh100 <handsomenyathi1@gmail.com>
2024-05-17 20:58:05 +0200

    remove individual function calls for buttons in html,message display

--- 05-Calculator/script.js
+++ 05-Calculator/script.js
@@ -1,19 +1,37 @@
+//constant variables
 const display = document.getElementById("display");
+const messageDisplay = document.getElementById("prediction");
+const calculatorButtons = document.getElementById("calculator-btns");
+const buttons = calculatorButtons.querySelectorAll("button");
 
-// Lets the selected number display on the input box
-function appendToDisplay(input){
-    display.value += input;
+//calculate function
+function calculate() {
+  if (display.value == "") {
+    messageDisplay.value = "Enter values to evaluate";
+    setTimeout(() => {
+      messageDisplay.value = "Calculator";
+    }, 2000);
+    return;
+  }
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "error";
+  }
+  console.log(display.value);
 }
 
-function clearDisplay(){
-    display.value = "";
-}
-
-function calculate(){
-    try{
-        display.value = eval(display.value);
+//add button value for each button clicked
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.value == "ac") {
+      display.value = "";
+      return;
     }
-    catch{
-        display.value = "error";
+    if (button.value == "=") {
+      calculate();
+      return;
     }
-}
+    display.value = display.value + button.value;
+  });
+});
