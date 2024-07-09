61aff1f7caf08594801b50765fba33f64e4125e2
Alexander-Agu <theonlyagu@gmail.com>
2024-07-09 14:05:49 +0200

    Re designing my Dev Notes Project

--- 09-Shopping Cart/script.js
+++ 09-Shopping Cart/script.js
@@ -1,39 +1,36 @@
 let shop = document.getElementById('shop');
 
 let shopItemsData = [{
-    id: "myword",
+    id: "myword1",
     name: "Casual Shirt",
     price: 45,
-    desc: ""
-}, {}, {}];
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+    img: "img/img-1.jpg"
+}, 
+{
+    id: "myword2",
+    name: "Office shirt",
+    price: 100,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+    img: "img/img-2.jpg"
+}, 
+{
+    id: "myword3",
+    name: "T-Shirt",
+    price: 25,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+    img: "img/img-3.jpg"
+},
+{
+    id: "myword4",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+    img: "img/img-4.jpg"
+}];
 
 let generateShop = () => {
-    return shop.innerHTML = `
-    <div class="item">
-    <img src="img/img-1.jpg" alt="" width="220">
-
-    <div class="details">
-        <h3>Casual Shirt</h3>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
-
-        <div class="price-quantity">
-            <h2>R45</h2>
-            
-            <div class="buttons">
-                <span class="material-symbols-outlined">
-                    remove
-                </span>
-
-                <div class="quantity">0</div>
-
-                <span class="material-symbols-outlined">
-                    add
-                </span>
-            </div>
-        </div>
-    </div>
-</div>
-    `
+    return (shop.innerHTML = shopItemsData.map())
 };
 
 generateShop();
