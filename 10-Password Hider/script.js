9a4ef46343cb7a7e3eed386e89225eafd0cf3473
Misterh100 <handsomenyathi1@gmail.com>
2024-07-16 20:42:54 +0200

    refactor: (09-Password Hider) add logic to hide and unhide password

--- 10-Password Hider/script.js
+++ 10-Password Hider/script.js
@@ -1,36 +1,51 @@
 // A password hider when you entering your password to prevent other people from viewing in
 
-const enterPassword = document.getElementById('js-enterPass');
-const reEnterPassword = document.getElementById('js-reEnterPass');
+const enterPassword = document.getElementById("js-enterPass");
+const reEnterPassword = document.getElementById("js-reEnterPass");
+const passHideButton = document.getElementById("js-pass-hide");
+const confirmHideButton = document.getElementById("js-confirm-hide");
 
+passHideButton.addEventListener("click", () => {
+  if (enterPassword.getAttribute("type") == "text") {
+    enterPassword.setAttribute("type", "password");
+    passHideButton.innerText = "unhide";
+  } else {
+    enterPassword.setAttribute("type", "text");
+    passHideButton.innerText = "hide";
+  }
+});
 
-let hideButton = document.getElementById('js-hide');
+confirmHideButton.addEventListener("click", () => {
+  if (reEnterPassword.getAttribute("type") == "text") {
+    reEnterPassword.setAttribute("type", "password");
+    confirmHideButton.innerText = "unhide";
+  } else {
+    reEnterPassword.setAttribute("type", "text");
+    confirmHideButton.innerText = "hide";
+  }
+});
 
-hideButton.addEventListener('click', (e)=>{
-    let initialValue = enterPassword.value
-    console.log(initialValue)
-    if(enterPassword.value){
-        enterPassword.value = hidePassword(enterPassword.value);
-    }
+// hideButton.addEventListener('click', (e)=>{
+//     let initialValue = enterPassword.value
+//     console.log(initialValue)
+//     if(enterPassword.value){
+//         enterPassword.value = hidePassword(enterPassword.value);
+//     }
 
-    else if(enterPassword.value !== initialValue){
-        enterPassword.value = initialValue;
-    }
-})
+//     else if(enterPassword.value !== initialValue){
+//         enterPassword.value = initialValue;
+//     }
+// })
 
+// // Changes Password to hidden Password
+// function hidePassword(password){
+//     let passwordLenth = password.length;
+//     let hiddenPassword = ['a','l','e','x'];
 
+//     hiddenPassword.forEach(function(names, index){
+//         console.log()
+//     });
 
+// };
 
-// Changes Password to hidden Password
-function hidePassword(password){
-    let passwordLenth = password.length;
-    let hiddenPassword = ['a','l','e','x'];
-
-    hiddenPassword.forEach(function(names, index){
-        console.log()
-    });
-
-};
-
-hidePassword("Alexander")
-
+// hidePassword("Alexander")
