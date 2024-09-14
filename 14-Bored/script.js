d662c3c9cd32dbd45c86f1a9a616c2fce32c918b
Alexander-Agu <theonlyagu@gmail.com>
2024-09-14 18:06:01 +0200

    finished the bored api project

--- 14-Bored/script.js
+++ 14-Bored/script.js
@@ -1,1 +1,41 @@
 // CONSTANT VARIABLES
+const showActivity = document.getElementById('showActivity');
+const acitvityInfo = document.getElementById('activityInfo');
+const getActivityBTN = document.getElementById('getActivity');
+
+
+
+const activityAPI = async () => {
+    try{
+        // Fetch data from an API
+        const fetchWeatherData = await fetch('http://bored.api.lewagon.com/api/activity/');
+
+        const boredActivity = await fetchWeatherData.json();
+        console.log(boredActivity)
+
+        return {
+        type: boredActivity.type,
+        activity: boredActivity.activity
+        }
+    } catch{
+        showActivity.textContent = 'Error Found';
+        acitvityInfo.textContent = 'Please try again'
+    }
+}
+
+function displayActivity(activity){
+    showActivity.textContent = activity.type;
+    acitvityInfo.textContent = activity.activity;
+}
+
+const main = async ()=> {
+  let details = await activityAPI();
+  displayActivity(details);
+  locationInput.focus();
+};
+
+getActivityBTN.addEventListener('click', ()=>{
+    let getInfo = activityAPI();
+
+    main()
+});
